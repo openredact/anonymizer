1d4faa43072a4a6f4e38eba4a5e73c0c008c9f00
langhabel <langhabel@users.noreply.github.com>
2020-06-04 12:55:08 +0200

    Add production dependency to setup.py

---
+++ anonymizer/__init__.py
@@ -0,0 +1,3 @@
+from anonymizer.anonymization.config import AnonymizerConfig  # noqa: F401
+from anonymizer.anonymization.anonymizer import Anonymizer  # noqa: F401
+from anonymizer.anonymization.pii import Pii, AnonymizedPii  # noqa: F401

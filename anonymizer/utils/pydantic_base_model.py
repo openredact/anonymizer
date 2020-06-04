b3a0cf9b7e0f46f51f6822b657f33f4bfc83ca71
langhabel <langhabel@users.noreply.github.com>
2020-06-04 13:22:10 +0200

    Replace docstring by comment
    Such that it is not converted to a schema description by fastAPI.

--- anonymizer/utils/pydantic_base_model.py
+++ anonymizer/utils/pydantic_base_model.py
@@ -7,7 +7,8 @@ def to_camel_case(snake_case):
 
 
 class CamelBaseModel(BaseModel):
-    """This base model automatically adds camelCase aliases."""
+    # This base model automatically adds camelCase aliases, that allow validation against camelCased json.
+    # Note: this is intentionally no docstring; fastAPI uses docstrings as description for OpenAPI schemas
 
     class Config:
         alias_generator = to_camel_case

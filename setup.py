d900cac36100daa466086348058d7298c0efd0e4
Pascal Berrang <contact@paberr.net>
2020-08-19 21:55:04 +0200

    Change name for pypi

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name="anonymizer",
+    name="or-anonymizer",
     version="0.1.0a",
     packages=find_packages(exclude=["tests"]),
     license="MIT",
     description="A Python module that provides multiple anonymization techniques for text.",
     long_description=open("README.md").read(),
-    install_requires=["pydantic==1.5.1", "numpy==1.18.5"],
+    install_requires=["pydantic==1.6.1", "numpy==1.19.1", "python-dateutil==2.8.1"],
 )

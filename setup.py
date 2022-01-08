017b481f0d1e2b10ac9f0d54a4846c7344450d3c
langhabel <langhabel@users.noreply.github.com>
2022-01-08 08:08:38 +0100

    Use version ranges in setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     license="MIT",
     description="A Python module that provides multiple anonymization techniques for text.",
     long_description=open("README.md").read(),
-    install_requires=["pydantic>=1.6.2", "numpy==1.19.1", "python-dateutil==2.8.1"],
+    install_requires=["pydantic>=1.6.2", "numpy>=1.19.1", "python-dateutil>=2.8.1"],
 )
